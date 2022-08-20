9409daf2e5476426d8edffd529e7bbfd84e01fde
muhammadkusuma <mwiraadekusuma1@gmail.com>
2022-08-20 21:11:09 +0700

    class, object, properties

---
+++ Properties.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once "data/Person.php";
+
+$person = new Person();
+$person->name="Wira";
+$person->address="Pekanbaru";
+
+
+var_dump($person);
+
+echo "Name : $person->name".PHP_EOL;
+echo "Address : $person->address".PHP_EOL;
+echo "Country : $person->country".PHP_EOL;
---
+++ data/Person.php
@@ -0,0 +1,7 @@
+<?php
+
+class Person{
+    var string $name;
+    var ?string $address=null;
+    var string $country="Indonesia";
+}
