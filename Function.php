2b15594b07f03d585891c6a45109dea355137550
muhammadkusuma <mwiraadekusuma1@gmail.com>
2022-08-21 14:20:46 +0700

    this Keyword

--- Function.php
+++ Function.php
@@ -3,5 +3,5 @@
 require_once "data/Person.php";
 
 $person = new Person();
-
+$person->name="Budi";
 $person->sayHello("Wira");
--- data/Person.php
+++ data/Person.php
@@ -5,7 +5,11 @@ class Person{
     var ?string $address=null;
     var string $country="Indonesia";
 
-    function sayHello(string $name){
-        echo "Hello $name".PHP_EOL;
+    function sayHello(?string $name){
+        if (is_null($name)){
+            echo "Hi, my name is {$this->name}".PHP_EOL ;
+        } else {
+            echo "Hello $name, my name is {$this->name}".PHP_EOL;
+        }
     }
 }
