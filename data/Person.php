ead6dec5abba51dbe35d3375674085a5c3ce50e8
muhammadkusuma <mwiraadekusuma1@gmail.com>
2022-08-21 14:14:55 +0700

    function

--- data/Person.php
+++ data/Person.php
@@ -4,4 +4,8 @@ class Person{
     var string $name;
     var ?string $address=null;
     var string $country="Indonesia";
+
+    function sayHello(string $name){
+        echo "Hello $name".PHP_EOL;
+    }
 }
