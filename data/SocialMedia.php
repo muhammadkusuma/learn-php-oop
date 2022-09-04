4a183f92817f01d3f0cae9339909c3eadf66acd0
muhammadkusuma <mwiraadekusuma1@gmail.com>
2022-09-04 08:35:56 +0700

    Final Function

--- data/SocialMedia.php
+++ data/SocialMedia.php
@@ -4,10 +4,14 @@ class SocialMedia{
     public string $name;
 }
 
-final class Facebook extends SocialMedia{
-
+class Facebook extends SocialMedia{
+    final public function login (string $username, string $password):bool{
+        return true;
+    }
 }
 
 class FakeFacebook extends Facebook{
-
+    public function login (string $username, string $password):bool{
+        return false;
+    }
 }
